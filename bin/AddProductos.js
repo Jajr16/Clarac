1ee3ac7d09e0946ed76741297b7f5c4fca35e1c5
Alfredo Jimenez <jajr0316@gmail.com>
2024-09-25 11:38:47 -0600

    Update AddProductos.js

--- bin/AddProductos.js
+++ bin/AddProductos.js
@@ -3,8 +3,6 @@ var Errores = require('./Error')
 
 function addProduct(req, callback) {
     const data = req.body
-    console.log(data)
-    console.log(data.Cod_Barras)
 
     db.query('insert into almacen values (?,?,?,?,?,?,?,?)', [data.Cod_Barras, data.Categoria, data.Articulo, data.Marca, data.Descripcion, data.Unidad, data.Existencia, 0], function (err2, result) {
         if (err2) { Errores(err2); return callback(err); } // Se hace un control de errores
